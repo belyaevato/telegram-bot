a4dd79c5ffff2a13e45de098aa89981fdc336110
belyaevato <belyaevato@gmail.com>
2026-03-20 09:52:30 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,25 +1,41 @@
 import asyncio
 import csv
 import os
+import threading
 
+from flask import Flask
 from aiogram import Bot, Dispatcher, F
 from aiogram.filters import CommandStart
 from aiogram.types import Message, CallbackQuery, InlineKeyboardMarkup, InlineKeyboardButton
 
+TOKEN = os.getenv("BOT_TOKEN")
+if not TOKEN:
+    raise ValueError("Переменная окружения BOT_TOKEN не задана")
 
-TOKEN = "8682117755:AAGVc07gLoZpJOltqpzKtFnHi40Yyx-dLXs"
 CHANNEL_ID = "@serdechnyy_marketolog"
 GIFT_LINK = "https://chatgpt.com/g/g-69af1c4e51f0819195f77b3237fe1ca1-tredmen"
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
+app = Flask(__name__)
 
-def save_user(user) -> None:
+
+@app.route("/")
+def home():
+    return "Bot is running!"
+
+
+def run_web():
+    port = int(os.getenv("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
+
+
+def save_user(user):
     file_name = "users.csv"
     file_exists = os.path.exists(file_name)
 
-    with open(file_name, "a", newline="", encoding="utf-8") as file:
+    with open(file_name, "a", newline="", encoding="utf-8-sig") as file:
         writer = csv.writer(file)
 
         if not file_exists:
@@ -44,17 +60,17 @@ async def start(message: Message):
     )
 
     await message.answer(
-    "Привет! 👋\n\n"
-    "Хочешь писать посты в Threads быстрее и получать больше вовлечения? 🔥\n\n"
-    "Я сделала бота, который:\n"
-    "— даёт сильные идеи под твою тему\n"
-    "— собирает цепляющую структуру\n"
-    "— оформляет текст так, чтобы его не пролистывали\n\n"
-    "👇 Забирай доступ:\n\n"
-    "1. Подпишись на канал\n"
-    "2. Нажми «Готово»",
-    reply_markup=keyboard
-)
+        "Привет! 👋\n\n"
+        "Хочешь писать посты в Threads быстрее и получать больше вовлечения? 🔥\n\n"
+        "Я сделала бота, который:\n"
+        "— даёт сильные идеи под твою тему\n"
+        "— собирает цепляющую структуру\n"
+        "— оформляет текст так, чтобы его не пролистывали\n\n"
+        "👇 Забирай доступ:\n\n"
+        "1. Подпишись на канал\n"
+        "2. Нажми «Готово»",
+        reply_markup=keyboard
+    )
 
 
 @dp.callback_query(F.data == "check_sub")
@@ -84,4 +100,5 @@ async def main():
 
 
 if __name__ == "__main__":
+    threading.Thread(target=run_web, daemon=True).start()
     asyncio.run(main())
